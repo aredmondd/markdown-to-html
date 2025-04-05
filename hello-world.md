cd87c502fceff95a506a3679c65c82e550d816de
Aiden Redmond <adnrdmnd@gmail.com>
2025-04-05 16:17:06 -0400

    read file from CLI test

---
+++ hello-world.md
@@ -0,0 +1,14 @@
+regular first line
+maybe we have a `code` block??
+another normal line
+another line with some `code`...
+now we have a
+
+---
+
+wow!
+glad we figured that out.
+
+---
+
+nice.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use std::fs::File;
+use std::io::{self, BufRead, BufReader};
+
+fn main() -> io::Result<()> {
+    // get args for file location
+    let args: Vec<String> = env::args().collect();
+
+    let file_name = &args[1];
+
+    let path = format!("{}.md", file_name);
+    let temp_path = format!("{}-translation.md", file_name);
+
+    println!("{}", path);
+    println!("{}", temp_path);
+
+    let file = File::open(path)?;
+    // let temp_file = File::create(temp_path)?;
+    let reader = BufReader::new(file);
+
+    for (line_number, line_result) in reader.lines().enumerate() {
+        let line = line_result?;
+        if let Some(index) = line.find("---") {
+            println!("Found '---' on line {} at index {}", line_number + 1, index);
+        }
+    }
+
+    Ok(())
 }
