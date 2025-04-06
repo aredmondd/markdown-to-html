dbe7c8f250a9e74c91f7fab4eed4a58842beb402
Aiden Redmond <adnrdmnd@gmail.com>
2025-04-06 13:44:26 -0400

    strikethrough, italics, and headings

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
+use regex::Regex;
 use std::fs::File;
 use std::io::prelude::*;
 use std::io::{self, BufRead, BufReader};
 
-const HR_TAG: &str = "<hr />";
-
 fn main() -> io::Result<()> {
+    // get the file name passed in from the CLI
     let file_name = match std::env::args().nth(1) {
         Some(name) => name,
         None => {
@@ -19,18 +19,6 @@ fn main() -> io::Result<()> {
     process_file(&input_path, &output_path)
 }
 
-fn transform_line(line: &str) -> String {
-    let mut line = line.replace("---", HR_TAG);
-
-    if line.trim() == HR_TAG {
-        line.push('\n');
-    } else {
-        line.push_str("<br />\n");
-    }
-
-    line
-}
-
 fn process_file(input_path: &str, output_path: &str) -> io::Result<()> {
     let file = File::open(input_path)?;
     let mut temp_file = File::create(output_path)?;
@@ -43,3 +31,59 @@ fn process_file(input_path: &str, output_path: &str) -> io::Result<()> {
 
     Ok(())
 }
+
+fn transform_line(line: &str) -> String {
+    let line = replace_hr(line);
+    let line = replace_headings(&line);
+    let line = replace_italics(&line);
+    let line = replace_strikethrough(&line);
+    // let line = replace_links(&line);
+    // let line = replace_inline_code(&line);
+    // let line = replace_code_block(&line);
+
+    // append a <br /> tag if necessary
+
+    line
+}
+
+fn replace_hr(line: &str) -> String {
+    line.replace("---", "<hr />")
+}
+
+fn replace_strikethrough(line: &str) -> String {
+    let re = Regex::new(r"~{2}(.+?)~{2}").unwrap();
+
+    re.replace_all(line, "<s>$1</s>").to_string()
+}
+
+fn replace_italics(line: &str) -> String {
+    let re = Regex::new(r"_(.+?)_").unwrap();
+
+    re.replace_all(line, "<em>$1</em>").to_string()
+}
+
+fn replace_headings(line: &str) -> String {
+    let re = Regex::new(r"^(#+)\s+(.+)$").unwrap();
+
+    re.replace_all(line, |caps: &regex::Captures| {
+        let num_hashes = caps.get(1).map_or(0, |m| m.as_str().len());
+
+        let tag = match num_hashes {
+            1 => "h1",
+            2 => "h2",
+            3 => "h3",
+            4 => "h4",
+            _ => "h3",
+        };
+
+        let heading_text = caps.get(2).map_or("", |m| m.as_str());
+
+        format!("<{}>{}</{}>", tag, heading_text, tag)
+    })
+    .to_string()
+}
+
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+// }
---
+++ test.md
@@ -0,0 +1,27 @@
+hello there
+
+today we are testing how my Rust `markdown-to-html` parser works, and if it _really works?_...
+
+---
+
+# here's an h1!
+## with an h2.
+### and an h3
+#### h4...
+##### h5
+###### h6
+
+i ~~hate~~ love this one!
+
+something else i [One Piece](https://en.wikipedia.org/wiki/One_Piece)
+
+```
+let mut person = String::from("Aiden Redmond");
+// code is awesome
+
+fn main() {
+	println!("Hello, World!);
+}
+```
+
+that's all for now!
